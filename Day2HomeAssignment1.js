b84c1d04e701fb5caf532226434118a9bcb9d73d
Govardhan1985 <41246007+Govardhan1985@users.noreply.github.com>
2026-06-09 17:50:35 -0700

    Add files via upload
    Week1Day2 Assignment

---
+++ Day2HomeAssignment1.js
@@ -0,0 +1,11 @@
+let userName = "gsurendran"
+let organization = "TCS"
+let contactNumber = 9884955591
+let knowsAutomation = true
+let usesPlaywright
+
+console.log("The user name = "+userName+ ", The type of identifier is - "+typeof(userName))
+console.log("User Works in - "+organization+", The type of identifier is - "+typeof(organization))
+console.log("User contact Number = "+contactNumber+", The type of identigfier is - "+typeof(contactNumber))
+console.log("Does the user knows Automation = "+knowsAutomation+ ", The type of identifier is - "+typeof(knowsAutomation))
+console.log("Does the user uses playwright = "+usesPlaywright+", The type of identifier is - "+typeof(usesPlaywright))
